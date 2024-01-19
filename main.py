6e66ed6625bca8d2345f23f2d94f2c4570b64806
Elias Campos <juninhocampos97@gmail.com>
2024-01-19 08:09:20 -0300

    atualizado pycache

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from bytebank import Funcionario
+from codigo.bytebank import Funcionario
 
 # lucas = Funcionario('Lucas da Silva', '18/09/1997', 1530)
 # print(lucas.idade())
 
-def teste_funcionario():
+def teste_idade():
     teste = Funcionario('Teste', '23/05/2000', 1250)
     print(f'Teste: {teste.idade()}')
 
-teste_funcionario()
+teste_idade()
